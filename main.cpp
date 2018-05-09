e9a24f0157e237f183841568aff5a4755df2fdbf
Kirill <kirulhik@yandex.ru>
2018-05-09 19:54:54 +0300

    Добавление программы main.cpp с последующим игнорированием файлов .o и .exe добавление файла .gitignore

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+/*программа является учебной*/
+int main()
+{
+    int x, y, sum, pr, razn, chastn;
+
+    setlocale(LC_ALL, "Russian");
+    cout << "Enter two numbers:\n ";
+    cin >> x >> y;
+
+    return 0;
+}
